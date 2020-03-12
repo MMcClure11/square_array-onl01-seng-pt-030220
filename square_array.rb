97123e94a4dacdbccfe3fa84a3f2495522fc1e3b
Megan Christine McClure <mcclme02@luther.edu>
2020-03-12 18:11:34 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,9 +3,13 @@
 #actions-->squares each element of an array-->squared integers
 
 
+# def square_array(array)
+#   numbers = []
+#   array.each do |number| numbers << number ** 2
+#   end
+#   numbers
+# end
+
 def square_array(array)
-  numbers = []
-  array.each do |number| numbers << number ** 2
-  end
-  numbers
+  array.collect {|element| element ** 2}
 end
